e103a720af060196b4b6e7c363c87a1a6e175846
Zsofi <zsofi.kisban@gmail.com>
2020-03-29 15:03:23 +0200

    Fooldalhoz hattervideo hozzaadva

--- onlab/app/src/main/java/hu/bme/aut/vizivandor/ui/home/HomeFragment.java
+++ onlab/app/src/main/java/hu/bme/aut/vizivandor/ui/home/HomeFragment.java
@@ -1,10 +1,13 @@
 package hu.bme.aut.vizivandor.ui.home;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.VideoView;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
@@ -17,19 +20,33 @@ import hu.bme.aut.vizivandor.R;
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    private VideoView mVideoView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
+        /*final TextView textView = root.findViewById(R.id.text_home);
         homeViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
+        });*/
+
+        mVideoView = (VideoView) root.findViewById(R.id.mVideoView);
+
+        Uri uri = Uri.parse("android.resource://"+ this.getContext().getPackageName() +"/"+R.raw.vizivandor_video);
+        mVideoView.setVideoURI(uri);
+        mVideoView.start();
+        mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                mediaPlayer.setLooping(true);
+            }
         });
+
         return root;
     }
 }
