0e2629f334be5f666bba4e46770c7281854eaf0e
Zsofi <zsofi.kisban@gmail.com>
2020-04-13 11:02:15 +0200

    Tura utvonalak expansion_panellel megoldva, kiindulas

---
+++ onlab/app/src/main/java/hu/bme/aut/vizivandor/ui/tura/ListTuraActivity.java
@@ -0,0 +1,37 @@
+package hu.bme.aut.vizivandor.ui.tura;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
+import hu.bme.aut.vizivandor.R;
+
+public class ListTuraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.turautvonal_informaciok);
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
--- onlab/app/src/main/java/hu/bme/aut/vizivandor/ui/tura/TuraFragment.java
+++ onlab/app/src/main/java/hu/bme/aut/vizivandor/ui/tura/TuraFragment.java
@@ -1,15 +1,14 @@
 package hu.bme.aut.vizivandor.ui.tura;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ImageButton;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import hu.bme.aut.vizivandor.R;
@@ -23,13 +22,28 @@ public class TuraFragment extends Fragment {
         turaViewModel =
                 ViewModelProviders.of(this).get(TuraViewModel.class);
         View root = inflater.inflate(R.layout.fragment_tura, container, false);
-        /*final TextView textView = root.findViewById(R.id.text_gallery);
-        turaViewModel.getText().observe(this, new Observer<String>() {
+
+
+
+        ImageButton FELSOTISZA = root.findViewById(R.id.btnFelsoTisza);
+        ImageButton TOKAJ = root.findViewById(R.id.btnTokajBodrog);
+        ImageButton DUNAKANYAR = root.findViewById(R.id.btnDunakanyar);
+        ImageButton SZIGETKOZ = root.findViewById(R.id.btnSzigetkoz);
+        ImageButton ALSODUNA = root.findViewById(R.id.btnAlsoDuna);
+        ImageButton KOROS = root.findViewById(R.id.btnKoros);
+        ImageButton TISZATO = root.findViewById(R.id.btnTiszaTo);
+
+
+
+        FELSOTISZA.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(final View view) {
+                Intent intent = new Intent(getActivity(), ListTuraActivity.class);
+                startActivity(intent);
             }
-        });*/
+        });
+
+
         return root;
     }
 }
