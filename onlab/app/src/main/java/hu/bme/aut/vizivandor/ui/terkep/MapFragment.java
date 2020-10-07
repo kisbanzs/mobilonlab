bbe66a486b11465da398294a5a1a379b2622cc3b
Zsofi <zsofi.kisban@gmail.com>
2020-10-07 18:08:35 +0200

    túra helyszinek kulonkulon megjelolhetok, osszes egyszerre fel és leveheto

--- onlab/app/src/main/java/hu/bme/aut/vizivandor/ui/terkep/MapFragment.java
+++ onlab/app/src/main/java/hu/bme/aut/vizivandor/ui/terkep/MapFragment.java
@@ -14,6 +14,7 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CompoundButton;
 import android.widget.ImageButton;
@@ -47,6 +48,7 @@ import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 import java.io.File;
 import java.util.ArrayList;
 
+import hu.bme.aut.vizivandor.MainActivity;
 import hu.bme.aut.vizivandor.R;
 
 import static android.os.Build.VERSION_CODES.M;
@@ -66,6 +68,7 @@ public class MapFragment extends Fragment implements LocationListener {
     private DirectedLocationOverlay locationOverlay;
     private static final int PERMISSAO_REQUERIDA = 1;
 
+    private ArrayList<GeoPoint> kezdo_allomasok = new ArrayList<GeoPoint>();
     private ArrayList<GeoPoint> felso_tisza = new ArrayList<GeoPoint>();
     private ArrayList<GeoPoint> tokaj_bodrogzug = new ArrayList<GeoPoint>();
     private ArrayList<GeoPoint> dunakanyar = new ArrayList<GeoPoint>();
@@ -74,7 +77,7 @@ public class MapFragment extends Fragment implements LocationListener {
     private ArrayList<GeoPoint> korosok = new ArrayList<GeoPoint>();
     private ArrayList<GeoPoint> tisza_to = new ArrayList<GeoPoint>();
 
-
+    private ArrayList<ArrayList<GeoPoint>> egesz_terkep = new ArrayList<ArrayList<GeoPoint>>();
 
 
     public MapFragment() {
@@ -132,33 +135,79 @@ public class MapFragment extends Fragment implements LocationListener {
         //osm.setPadding(5,5,5,5);
 
 
+        //7db túra kezdő állomásainak helye térképen jelölve kék kocsival
+
+        kezdo_allomasok.add(new GeoPoint(48.104927,22.829234));
+        kezdo_allomasok.add(new GeoPoint(48.137781,21.400946));
+        kezdo_allomasok.add(new GeoPoint(47.789443,18.731162));
+        kezdo_allomasok.add(new GeoPoint(47.940391,17.357821));
+        kezdo_allomasok.add(new GeoPoint(46.592602,18.860733));
+        kezdo_allomasok.add(new GeoPoint(46.759521,21.153568));
+        kezdo_allomasok.add(new GeoPoint(47.645895,20.660313));
+
+        felso_tisza.add(new GeoPoint(48.099701,22.624464));
+        felso_tisza.add(new GeoPoint(48.061861,22.519040));
+        felso_tisza.add(new GeoPoint(48.127612,22.340048));
+
+        tokaj_bodrogzug.add(new GeoPoint(48.167749,21.398657));
+        tokaj_bodrogzug.add(new GeoPoint(48.413187,21.637227));
+        tokaj_bodrogzug.add(new GeoPoint(48.360649,21.693310));
+        tokaj_bodrogzug.add(new GeoPoint(48.315088,21.568245));
+
+        dunakanyar.add(new GeoPoint(47.765539,18.917588));
+        dunakanyar.add(new GeoPoint(47.682486,19.085478));
+        dunakanyar.add(new GeoPoint(47.571031,19.065174));
+
+        szigetkoz.add(new GeoPoint(47.901520,17.425963));
+        szigetkoz.add(new GeoPoint(47.835826,17.515642));
+
+        also_duna.add(new GeoPoint(46.497939,18.916054));
+        also_duna.add(new GeoPoint(46.199714,18.826217));
+        also_duna.add(new GeoPoint(46.172750,18.940883));
+
+        korosok.add(new GeoPoint(46.882307,21.031375));
+        korosok.add(new GeoPoint(46.936571,20.836434));
+        korosok.add(new GeoPoint(46.861703,20.539445));
+
+        tisza_to.add(new GeoPoint(47.624602,20.745482));
+        tisza_to.add(new GeoPoint(47.645770,20.660322));
+
+        egesz_terkep.add(kezdo_allomasok);
+        egesz_terkep.add(felso_tisza);
+        egesz_terkep.add(tokaj_bodrogzug);
+        egesz_terkep.add(dunakanyar);
+        egesz_terkep.add(szigetkoz);
+        egesz_terkep.add(also_duna);
+        egesz_terkep.add(korosok);
+        egesz_terkep.add(tisza_to);
+
+
+
+
 
         //ALSO MENÜ 3 GOMBJAINAK FELADATA:
-        //INDULÓ ÁLLOMÁSOK GOMBJA
+        //1 INDULÓ ÁLLOMÁSOK GOMBJA
         ToggleButton toggle = (ToggleButton) view.findViewById(R.id.simpleSwitch);
         toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
 
-                //7db túra kezdő állomásainak helye térképen jelölve kék kocsival
-                ArrayList<GeoPoint> kezdo_allomasok = new ArrayList<GeoPoint>();
-                kezdo_allomasok.add(new GeoPoint(48.104927,22.829234));
-                kezdo_allomasok.add(new GeoPoint(48.137781,21.400946));
-                kezdo_allomasok.add(new GeoPoint(47.789443,18.731162));
-                kezdo_allomasok.add(new GeoPoint(47.940391,17.357821));
-                kezdo_allomasok.add(new GeoPoint(46.592602,18.860733));
-                kezdo_allomasok.add(new GeoPoint(46.759521,21.153568));
-                kezdo_allomasok.add(new GeoPoint(47.645895,20.660313));
-
                 if (isChecked) {
+
                     for(GeoPoint g: kezdo_allomasok){
                         addMarker(g);
                     }
+
+                    for(int i=1; i<egesz_terkep.size(); i++){
+                        for(GeoPoint g: egesz_terkep.get(i)){
+                            addPotty(g);
+                        }
+                    }
+
+
                     System.out.println("kocsi gomb ON");
                 } else {
-                    for(GeoPoint g: kezdo_allomasok){
-                        removeMarker();
-                    }
-                    System.out.println("kocsi gomb OFF");
+                    removeMarker();
+
 
                 }
             }
@@ -166,7 +215,7 @@ public class MapFragment extends Fragment implements LocationListener {
 
 
 
-        //AKTUÁLIS POZÍCIÓ GOMBJA
+        //2 AKTUÁLIS POZÍCIÓ GOMBJA
         ImageButton btnCurrentLocation = view.findViewById(R.id.button_current_location);
         btnCurrentLocation.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -186,15 +235,96 @@ public class MapFragment extends Fragment implements LocationListener {
             }
         });
 
-        //7 DB TÚRA HELYSZÍN KIVÁLASZTÓ GOMBJA
+        //3 A 7 DB TÚRA HELYSZÍN KIVÁLASZTÓ GOMBJA
 
-        String [] values =
-                {"A Felső-Tisza","Tokaj és a Bodrogzug","Dunakanyar túra","Szigetköz szépségei","Alsó-Duna","Körösök","Tisza-tó"};
+        String [] values = {"A Felső-Tisza","Tokaj és a Bodrogzug","Dunakanyar túra","Szigetköz szépségei","Alsó-Duna","Körösök","Tisza-tó"};
         Spinner spinner_turak = (Spinner) view.findViewById(R.id.spinner_turak);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, values);
         adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
         spinner_turak.setAdapter(adapter);
         System.out.println(String.valueOf(spinner_turak.getSelectedItem()));
+
+
+
+
+        spinner_turak.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+            {
+                // Get Selected Class name from the list
+                String selectedValues = parent.getItemAtPosition(position).toString();
+
+                switch (selectedValues)
+                {
+                    case "A Felső-Tisza":
+                        removeMarker();
+                        for(GeoPoint g: felso_tisza){
+                            addPotty(g);
+                        }
+                        break;
+
+                    case "Tokaj és a Bodrogzug":
+                        removeMarker();
+                        for(GeoPoint g: tokaj_bodrogzug){
+                            addPotty(g);
+                        }
+                        break;
+
+                    case "Dunakanyar túra":
+                        removeMarker();
+                        for(GeoPoint g: dunakanyar){
+                            addPotty(g);
+                        }
+                        break;
+
+                    case "Szigetköz szépségei":
+                        removeMarker();
+                        for(GeoPoint g: szigetkoz){
+                            addPotty(g);
+                        }
+                        break;
+
+                    case "Alsó-Duna":
+                        removeMarker();
+                        for(GeoPoint g: also_duna){
+                            addPotty(g);
+                        }
+                        break;
+
+                    case "Körösök":
+                        removeMarker();
+                        for(GeoPoint g: korosok){
+                            addPotty(g);
+                        }
+                        break;
+
+                    case "Tisza-tó":
+                        removeMarker();
+                        for(GeoPoint g: tisza_to){
+                            addPotty(g);
+                        }
+                        break;
+
+
+                    default:
+                        System.out.println("ez itt valami maaaaaas");
+                        break;
+
+                }
+
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent)
+            {
+
+            }
+        });
+
+
+
 
 
         //Lagymanyosi obol geo pontja
@@ -205,71 +335,11 @@ public class MapFragment extends Fragment implements LocationListener {
 
 
 
-        //feltoltom az allomasok helyzeteivel a 7 listát
-
-            felso_tisza.add(new GeoPoint(48.099701,22.624464));
-            felso_tisza.add(new GeoPoint(48.061861,22.519040));
-            felso_tisza.add(new GeoPoint(48.127612,22.340048));
-        for(GeoPoint g: felso_tisza){
-            addPotty(g);
-        }
-
-            tokaj_bodrogzug.add(new GeoPoint(48.167749,21.398657));
-            tokaj_bodrogzug.add(new GeoPoint(48.413187,21.637227));
-            tokaj_bodrogzug.add(new GeoPoint(48.360649,21.693310));
-            tokaj_bodrogzug.add(new GeoPoint(48.315088,21.568245));
-        for(GeoPoint g: tokaj_bodrogzug){
-            addPotty(g);
-        }
-
-            dunakanyar.add(new GeoPoint(47.765539,18.917588));
-            dunakanyar.add(new GeoPoint(47.682486,19.085478));
-            dunakanyar.add(new GeoPoint(47.571031,19.065174));
-        for(GeoPoint g: dunakanyar){
-            addPotty(g);
-        }
-
-
-            szigetkoz.add(new GeoPoint(47.901520,17.425963));
-            szigetkoz.add(new GeoPoint(47.835826,17.515642));
-        for(GeoPoint g: szigetkoz){
-            addPotty(g);
-        }
-
-
-            also_duna.add(new GeoPoint(46.497939,18.916054));
-            also_duna.add(new GeoPoint(46.199714,18.826217));
-            also_duna.add(new GeoPoint(46.172750,18.940883));
-        for(GeoPoint g: also_duna){
-            addPotty(g);
-        }
-
-
-            korosok.add(new GeoPoint(46.882307,21.031375));
-            korosok.add(new GeoPoint(46.936571,20.836434));
-            korosok.add(new GeoPoint(46.861703,20.539445));
-        for(GeoPoint g: korosok){
-            addPotty(g);
-        }
-
-
-            tisza_to.add(new GeoPoint(47.624602,20.745482));
-            tisza_to.add(new GeoPoint(47.645770,20.660322));
-        for(GeoPoint g: tisza_to){
-            addPotty(g);
-        }
-
-
-
-
         locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
         //locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000, 0, this);
 
 
-    /* grade no mapa
-    LatLonGridlineOverlay2 overlay = new LatLonGridlineOverlay2();
-    osm.getOverlays().add(overlay);
-    */
+
 
         osm.setMapListener(new MapListener() {
             @Override
